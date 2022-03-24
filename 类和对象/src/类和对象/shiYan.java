f888ca1ff2cb3f1a34c2d8d308f1465f6e4b2aab
ILoveAstesia <1324677263@qq.com>
2022-03-24 08:37:45 +0800

    类和对象的实验

--- 类和对象/src/类和对象/shiYan.java
+++ 类和对象/src/类和对象/shiYan.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 class Rectangle
 {
-	double width,height;
-	String color;
+	private double width,height;
+	private String color;
 	
 	Rectangle (){
 		width=10.0;
@@ -23,6 +23,7 @@ class Rectangle
 	public void getter() {
 		System.out.print("宽："+width+"\t"+"高："+height+"\t"+"颜色："+color+"\n");
 	}
+	
 	public void setter() {
 		System.out.print("高：");
 		Scanner input_h=new Scanner(System.in);		
@@ -33,15 +34,31 @@ class Rectangle
 		System.out.print("颜色：");
 		Scanner input_c=new Scanner(System.in);
 		String color=input_c.next();
+		
 		this.color=color;
 		
 		this.height=height;
 	
-		this.width=width;		
+		this.width=width;
+
 		input_w.close ();
 		input_c.close ();
 		input_h.close ();
 	}
+
+	public void set_color(String color_1) 
+	{
+		color=color_1;
+	}
+	public void set_height(double height) 
+	{
+		this.height=height;
+	}
+	public void set_width(double width) 
+	{
+		this.width=width;
+	}
+	
 	public static void getArea(Rectangle a) {
 		System.out.print("面积为:"+a.height*a.width+"\n");
 	}
@@ -50,26 +67,22 @@ class Rectangle
 public class shiYan {
 
 	public static void main(String[] args) {
-		int i=0;
-		switch(i)
-		{
-		
-		}
-
-
 		// TODO 自动生成的方法存根
 		//Rectangle a= new Rectangle ();
-		Rectangle b= new Rectangle (5.0,5.0,"Astesia");
-		//Rectangle.getArea(a);
-		b.setter();
-		b.getter();
-		Rectangle.getArea(b);
+		Rectangle a= new Rectangle ();
+//		Rectangle b= new Rectangle (5.0,5.0,"Astesia");
+		a.set_color("Maple");
+		a.set_width(10.0);
+		a.set_height(10.0);
+		a.getter();
+		Rectangle.getArea(a);
+//		b.setter();
+//		b.getter();
+//		Rectangle.getArea(b);
 		//go to git hub
 		//go to git hub
 
 	}
-	
-
 }
 
 
